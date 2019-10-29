8a2948e25c5f4762200c55cf93a7b9f7def40ade
Jeremy Wallace <jeremygw@amazon.com>
2019-10-29 01:21:49 -0700

    changes

---
+++ robot_ws/build/dinoapp/catkin_generated/setup_py_interrogation.cmake
@@ -0,0 +1,6 @@
+set(dinoapp_SETUP_PY_VERSION "1.2.0")
+set(dinoapp_SETUP_PY_SCRIPTS "")
+set(dinoapp_SETUP_PY_PACKAGES "dinoapp")
+set(dinoapp_SETUP_PY_PACKAGE_DIRS "src/dinoapp")
+set(dinoapp_SETUP_PY_MODULES "")
+set(dinoapp_SETUP_PY_MODULE_DIRS "")
